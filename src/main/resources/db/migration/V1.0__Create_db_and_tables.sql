2cab6343222f51be39f677a3f7f22741cc130fac
Nikita Koshelev <39601787+HuKuTa94@users.noreply.github.com>
2021-02-02 00:21:01 +0500

    added table 'todo_list'

--- src/main/resources/db/migration/V1.0__Create_db_and_tables.sql
+++ src/main/resources/db/migration/V1.0__Create_db_and_tables.sql
@@ -1,3 +1,10 @@
 CREATE DATABASE IF NOT EXISTS `vdcom-test-task` DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_general_ci;
 
 USE `vdcom-test-task`;
+
+CREATE TABLE IF NOT EXISTS `todo_list` (
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `text` VARCHAR(255) NOT NULL,
+    `is_done` TINYINT NOT NULL,
+    PRIMARY KEY (id)
+) ENGINE = INNODB;
